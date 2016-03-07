18e3b8abff40a25771589f1342b34ff1aa46f8e5
Tim Padjen <tpadjen@gmail.com>
2016-03-07 13:40:11 -0700

    Add ng2-material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import {Component} from 'angular2/core';
-
+import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
 
 @Component({
   selector: 'app',
   template: require('./app.html'),
-  styles: [require('./app.scss')]
+  styles: [require('./app.scss')],
+  directives: [MATERIAL_DIRECTIVES]
 })
 export class App { }

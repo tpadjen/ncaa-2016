30784bb5057b1a1deb2345ecf346d726a21eeb32
Tim Padjen <tpadjen@gmail.com>
2016-03-13 09:19:32 -0600

    Use hash strategy in development but path strategy in production

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
-import {enableProdMode} from 'angular2/core';
+import {enableProdMode, provide} from 'angular2/core';
 import {bootstrap} from 'angular2/bootstrap';
 import {MATERIAL_PROVIDERS} from 'ng2-material/all';
 import {App} from './app/app';
 import {FantasyTeamService} from './app/fantasyTeams/fantasy-team.service';
 import {SchoolService} from './app/schools/school.service';
 import {DraftService} from './app/draft/draft.service';
-import {ROUTER_PROVIDERS} from 'angular2/router';
+import {
+  ROUTER_PROVIDERS,
+  LocationStrategy,
+  HashLocationStrategy,
+  PathLocationStrategy
+} from 'angular2/router';
+
+let APP_LOCATION_STRATEGY = provide(LocationStrategy, {useClass: HashLocationStrategy});
 
 declare let __PRODUCTION__: any;
 if (__PRODUCTION__) {
   enableProdMode();
+  APP_LOCATION_STRATEGY = provide(LocationStrategy, {useClass: PathLocationStrategy});
 }
 
 bootstrap(App, [
   ROUTER_PROVIDERS,
+  APP_LOCATION_STRATEGY,
   MATERIAL_PROVIDERS,
   SchoolService,
   FantasyTeamService,

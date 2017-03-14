57e594eb40e8cb57b0b3b869a66b99996be1d7c2
Tim Padjen <tpadjen@gmail.com>
2017-03-14 07:55:28 -0600

    Update typings syntax

--- typings/browser.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="browser/ambient/es6-shim/index.d.ts" />
-/// <reference path="browser/ambient/firebase/index.d.ts" />
-/// <reference path="browser/ambient/zone.js/index.d.ts" />
-/// <reference path="browser/definitions/zone.js/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/es6-shim/index.d.ts" />
-/// <reference path="main/ambient/firebase/index.d.ts" />
-/// <reference path="main/ambient/zone.js/index.d.ts" />
-/// <reference path="main/definitions/zone.js/index.d.ts" />

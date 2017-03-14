d2a6dcb1319735a81114f937eb1ba5eb070fa94c
Tim Padjen <tpadjen@gmail.com>
2017-03-14 09:01:25 -0600

    Update year

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const DRAFT_NAME = '2016';
-export const DRAFT_URL = 'https://mvhs-ncaa-2016.firebaseio.com/';
+export const DRAFT_NAME = '2017';
+export const DRAFT_URL = 'https://mvhs-ncaa-2017.firebaseio.com/';
 export const NUMBER_OF_FANTASY_TEAMS = 8;
